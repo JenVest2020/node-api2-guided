d5c6eb307fcd9043d950b173f56744ae5b9e28b5
JenVest2020 <jenvest2019@gmail.com>
2020-09-03 22:32:11 -0500

    learning query in router

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -34,6 +34,21 @@ router.get('/:id', (req, res) => {
         });
 });
 
+router.get('/:id/messages', (req, res) => {
+    Hubs.findHubMessages(req.params.id)
+        .then(messages => {
+            if (messages.length > 0) {
+                res.status(200).json(messages);
+            } else {
+                res.status(404).json({ message: 'no messages for this hub' });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: 'error getting messages', error });
+        })
+});
+
 router.post('/', (req, res) => {
     Hubs.add(req.body)
         .then(hub => {
